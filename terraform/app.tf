d2802f47347041034063ef4f5ed6267678e0ba36
Matthew Garber <matthew.garber@contino.io>
2021-06-29 22:13:23 -0400

    updates

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,18 +1,25 @@
 resource "digitalocean_app" "flask_example" {
   spec {
-    name   = "flask_sample"
+    name   = "flask-sample"
     region = "nyc1"
 
     service {
       name               = "go-service"
       environment_slug   = "python"
       instance_count     = 1
-      instance_size_slug = "s-1vcpu-1gb"
+      instance_size_slug = "basic-xxs"
 
-      git {
-        repo_clone_url = "https://github.com/mgarber-ops/digi-o-flask-app"
+      github {
+        repo = "mgarber-ops/digi-o-flask-app"
         branch         = "main"
+        deploy_on_push = "true"
       }
+
+    http_port = 8080
+    routes {
+    path = "/"
     }
+    }
+    
   }
 }
